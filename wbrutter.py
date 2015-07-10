ae08ffc58d82fbce3eef12e1220ffec4588760df
Ronan Kervella <ronan.kervella@gmail.com>
2015-07-10 09:24:36 -1000

    First commit

---
+++ wbrutter.py
@@ -0,0 +1,15 @@
+#coding : utf-8
+
+import request
+
+class Brutter():
+
+    def __init__(self, host, port=80):
+        self.hostname = host
+        self.port = port
+
+    def run(self):
+        pass
+
+if __name__ == '__main__':
+    pass
